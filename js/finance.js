98be83085432f33f7c86e2d66ae4bee48588b318
Oleksandr Vashchishak <q121312q@gmail.com>
2020-11-05 08:56:49 +0200

    change

---
+++ js/finance.js
@@ -0,0 +1,30 @@
+let curencyAccaunt = document.querySelector('.finance-open__currency-block')
+let iconFinance = document.querySelector('.select__icon-finance')
+curencyAccaunt.addEventListener('click', openCurrency)
+function openCurrency(){
+  curencyAccaunt.classList.toggle('active')
+  iconFinance.classList.toggle('active')
+}
+
+
+document.addEventListener("click", function(event) {
+  let target = event.target;
+if(target.classList.contains('finance-open__currency-block')){
+  return
+}
+if(target.classList.contains('finance-open__course-part')){
+  return
+}
+if(target.classList.contains('finance-open__course')){
+  return
+}
+if(target.classList.contains('select__icon-finance')){
+  return
+}
+if(target.classList.contains('finance-open__number')){
+  return
+}
+
+ curencyAccaunt.classList.remove('active')
+ iconFinance.classList.remove('active')
+});
