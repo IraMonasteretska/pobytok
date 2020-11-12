4d6086f9294885cecbf436dd7f3d364dd0eb266f
Oleksandr Vashchishak <q121312q@gmail.com>
2020-11-12 14:12:51 +0200

    add realty

--- js/tabs-account.js
+++ js/tabs-account.js
@@ -11,150 +11,185 @@
 //   document.getElementById(cityName).style.display = "block";
 //   evt.currentTarget.className += " active";
 // }
-  // document.getElementById("defaultOpenTab").click();
+// document.getElementById("defaultOpenTab").click();
 
+let backgrountOpacityAll = document.querySelector(".backgrount-opacity-all");
+let accountMobileMenu = document.querySelector(".personal__mobile-menu");
+let accountMobileMenuPanel = document.querySelector(".personal__panel");
+let closeMobileAcc = document.querySelector(".close-mobile-acc");
 
-let backgrountOpacityAll = document.querySelector('.backgrount-opacity-all');
-let accountMobileMenu = document.querySelector('.personal__mobile-menu');
-let accountMobileMenuPanel = document.querySelector('.personal__panel');
-let closeMobileAcc = document.querySelector('.close-mobile-acc');
-
-
-
-
-accountMobileMenu.addEventListener('click', getMobileMenuAcc)
-function getMobileMenuAcc(){
-
-  accountMobileMenuPanel.classList.add('active')
-  backgrountOpacityAll.classList.add('active')
+accountMobileMenu.addEventListener("click", getMobileMenuAcc);
+function getMobileMenuAcc() {
+  accountMobileMenuPanel.classList.add("active");
+  backgrountOpacityAll.classList.add("active");
 }
-closeMobileAcc.addEventListener('click', closeMobileMenuAcc)
-function closeMobileMenuAcc(){
-  accountMobileMenuPanel.classList.remove('active')
-  backgrountOpacityAll.classList.remove('active')
-
+closeMobileAcc.addEventListener("click", closeMobileMenuAcc);
+function closeMobileMenuAcc() {
+  accountMobileMenuPanel.classList.remove("active");
+  backgrountOpacityAll.classList.remove("active");
 }
 
-
-if(document.querySelector('.personal__real-icon-play')){
-
-let iconPlay = document.querySelector('.personal__real-icon-play');
-let realColPlay  = document.querySelector('.personal__real-col-play ');
-let realColPause = document.querySelector('.personal__real-col-pause');
-iconPlay.addEventListener('click', function(){
-realColPlay.classList.toggle('block')
-realColPause.classList.toggle('block')
-});
-
+if (document.querySelector(".personal__real-icon-play")) {
+  let iconPlay = document.querySelector(".personal__real-icon-play");
+  let realColPlay = document.querySelector(".personal__real-col-play ");
+  let realColPause = document.querySelector(".personal__real-col-pause");
+  iconPlay.addEventListener("click", function () {
+    realColPlay.classList.toggle("block");
+    realColPause.classList.toggle("block");
+  });
 }
 
-
-
-
-document.addEventListener('DOMContentLoaded', createSelect, false);
+document.addEventListener("DOMContentLoaded", createSelect, false);
 function createSelect() {
-    var select = document.getElementsByTagName('select'),
-      liElement,
-      ulElement,
-      optionValue,
-      iElement,
-      optionText,
-      selectDropdown,
-      elementParentSpan;
+  var select = document.getElementsByTagName("select"),
+    liElement,
+    ulElement,
+    optionValue,
+    iElement,
+    optionText,
+    selectDropdown,
+    elementParentSpan;
 
-      for (var select_i = 0, len = select.length; select_i < len; select_i++) {
-        //console.log('selects init');
+  for (var select_i = 0, len = select.length; select_i < len; select_i++) {
+    //console.log('selects init');
 
-      select[select_i].style.display = 'none';
-      wrapElement(document.getElementById(select[select_i].id), document.createElement('div'), select_i, select[select_i].getAttribute('placeholder-text'));
+    select[select_i].style.display = "none";
+    wrapElement(
+      document.getElementById(select[select_i].id),
+      document.createElement("div"),
+      select_i,
+      select[select_i].getAttribute("placeholder-text")
+    );
 
-      for (var i = 0; i < select[select_i].options.length; i++) {
-        liElement = document.createElement("li");
-        optionValue = select[select_i].options[i].value;
-        optionText = document.createTextNode(select[select_i].options[i].text);
-        liElement.className = 'select-dropdown__list-item';
-        liElement.setAttribute('data-value', optionValue);
-        liElement.appendChild(optionText);
-        ulElement.appendChild(liElement);
+    for (var i = 0; i < select[select_i].options.length; i++) {
+      liElement = document.createElement("li");
+      optionValue = select[select_i].options[i].value;
+      optionText = document.createTextNode(select[select_i].options[i].text);
+      liElement.className = "select-dropdown__list-item";
+      liElement.setAttribute("data-value", optionValue);
+      liElement.appendChild(optionText);
+      ulElement.appendChild(liElement);
 
-        liElement.addEventListener('click', function () {
+      liElement.addEventListener(
+        "click",
+        function () {
           displyUl(this);
-        }, false);
+        },
+        false
+      );
+    }
+  }
+  function wrapElement(el, wrapper, i, placeholder) {
+    el.parentNode.insertBefore(wrapper, el);
+    wrapper.appendChild(el);
+
+    document.addEventListener("click", function (e) {
+      let clickInside = wrapper.contains(e.target);
+      if (!clickInside) {
+        let menu = wrapper.getElementsByClassName("select-dropdown__list");
+        menu[0].classList.remove("active");
+        menu[0].parentNode.children[1].classList.remove("active");
+      }
+    });
+
+    var buttonElement = document.createElement("button"),
+      spanElement = document.createElement("span"),
+      spanText = document.createTextNode(placeholder);
+    iElement = document.createElement("i");
+    ulElement = document.createElement("ul");
+
+    wrapper.className = "select-dropdown select-dropdown--" + i;
+    buttonElement.className =
+      "select-dropdown__button select-dropdown__button--" + i;
+    buttonElement.setAttribute("data-value", "");
+    buttonElement.setAttribute("type", "button");
+    spanElement.className = "select-dropdown select-dropdown--" + i;
+    iElement.className = "zmdi zmdi-chevron-down";
+    ulElement.className = "select-dropdown__list select-dropdown__list--" + i;
+    ulElement.id = "select-dropdown__list-" + i;
+
+    wrapper.appendChild(buttonElement);
+    spanElement.appendChild(spanText);
+    buttonElement.appendChild(spanElement);
+    buttonElement.appendChild(iElement);
+    wrapper.appendChild(ulElement);
+  }
+
+  function displyUl(element) {
+    if (element.tagName == "BUTTON") {
+      selectDropdown = element.parentNode.getElementsByTagName("ul");
+      for (var i = 0, len = selectDropdown.length; i < len; i++) {
+        selectDropdown[i].classList.toggle("active");
+        element.classList.toggle("active");
+      }
+    } else if (element.tagName == "LI") {
+      var selectId = element.parentNode.parentNode.getElementsByTagName(
+        "select"
+      )[0];
+      selectElement(selectId.id, element.getAttribute("data-value"));
+      elementParentSpan = element.parentNode.parentNode.getElementsByTagName(
+        "span"
+      );
+      element.parentNode.classList.toggle("active");
+      if(element.parentNode.classList.contains('select-dropdown__list--6')){
+      let cityPolish = document.querySelectorAll(".select-custom-acc-v");
+      for (i = 0; i < cityPolish.length; i++) {
+        cityPolish[i].classList.remove("active");
+      }
+      for (i = 0; i < cityPolish.length; i++) {
+        if (element.getAttribute("data-value") == i + 2) {
+          cityPolish[i].classList.add("active");
+          break;
+        }
       }
     }
-    function wrapElement(el, wrapper, i, placeholder) {
-      el.parentNode.insertBefore(wrapper, el);
-      wrapper.appendChild(el);
-
-      document.addEventListener('click', function (e) {
-        let clickInside = wrapper.contains(e.target);
-        if (!clickInside) {
-          let menu = wrapper.getElementsByClassName('select-dropdown__list');
-          menu[0].classList.remove('active');
-        }
-      });
-
-      var buttonElement = document.createElement("button"),
-        spanElement = document.createElement("span"),
-        spanText = document.createTextNode(placeholder);
-        iElement = document.createElement("i");
-        ulElement = document.createElement("ul");
-
-      wrapper.className = 'select-dropdown select-dropdown--' + i;
-      buttonElement.className = 'select-dropdown__button select-dropdown__button--' + i;
-      buttonElement.setAttribute('data-value', '');
-      buttonElement.setAttribute('type', 'button');
-      spanElement.className = 'select-dropdown select-dropdown--' + i;
-      iElement.className = 'zmdi zmdi-chevron-down';
-      ulElement.className = 'select-dropdown__list select-dropdown__list--' + i;
-      ulElement.id = 'select-dropdown__list-' + i;
-
-      wrapper.appendChild(buttonElement);
-      spanElement.appendChild(spanText);
-      buttonElement.appendChild(spanElement);
-      buttonElement.appendChild(iElement);
-      wrapper.appendChild(ulElement);
+      element.parentNode.parentNode.children[1].classList.toggle("active");
+      element.parentNode.parentNode.children[1].style.color = "#464646";
+      elementParentSpan[0].textContent = element.textContent;
+      elementParentSpan[0].parentNode.setAttribute(
+        "data-value",
+        element.getAttribute("data-value")
+      );
     }
+  }
+  function selectElement(id, valueToSelect) {
+    var element = document.getElementById(id);
+    element.value = valueToSelect;
+    element.setAttribute("selected", "selected");
+  }
+  var buttonSelect = document.getElementsByClassName("select-dropdown__button");
 
-    function displyUl(element) {
+  for (var i = 0, len = buttonSelect.length; i < len; i++) {
+    buttonSelect[i].addEventListener(
+      "click",
+      function (e) {
+        e.preventDefault();
+        displyUl(this);
+      },
+      false
+    );
+  }
+  //     let buttonSelectQuery = document.querySelector('.select-dropdown__button');
+  // buttonSelectQuery.addEventListener('click', getSelectMenu)
 
-      if (element.tagName == 'BUTTON') {
-        selectDropdown = element.parentNode.getElementsByTagName('ul');
-        //var labelWrapper = document.getElementsByClassName('js-label-wrapper');
-        for (var i = 0, len = selectDropdown.length; i < len; i++) {
-          selectDropdown[i].classList.toggle("active");
-          //var parentNode = $(selectDropdown[i]).closest('.js-label-wrapper');
-          //parentNode[0].classList.toggle("active");
-        }
-      } else if (element.tagName == 'LI') {
-        var selectId = element.parentNode.parentNode.getElementsByTagName('select')[0];
-        selectElement(selectId.id, element.getAttribute('data-value'));
-        elementParentSpan = element.parentNode.parentNode.getElementsByTagName('span');
-        element.parentNode.classList.toggle("active");
-        elementParentSpan[0].textContent = element.textContent;
-        elementParentSpan[0].parentNode.setAttribute('data-value', element.getAttribute('data-value'));
-      }
-
-    }
-    function selectElement(id, valueToSelect) {
-      var element = document.getElementById(id);
-      element.value = valueToSelect;
-      element.setAttribute('selected', 'selected');
-    }
-    var buttonSelect = document.getElementsByClassName('select-dropdown__button');
-   
-    for (var i = 0, len = buttonSelect.length; i < len; i++) {
-      buttonSelect[i].addEventListener('click', function (e) {
-				e.preventDefault();
-				displyUl(this);
-			}, false);
-    }
-    let buttonSelectQuery = document.querySelector('.select-dropdown__button');
-buttonSelectQuery.addEventListener('click', getSelectMenu)
-
-function getSelectMenu(){
-  buttonSelectQuery.classList.toggle('active')
-}
+  // function getSelectMenu(){
+  //   buttonSelectQuery.classList.toggle('active')
+  // }
 }
 
+let photoLabel = document.querySelector(".personal__add-photo-label");
+let bunnerLabel = document.querySelector(".personal__add-bunner-label");
+let leadBunner = document.querySelector(".personal__lead-bunner");
+let leadPhoto = document.querySelector(".personal__lead-photo");
 
+photoLabel.addEventListener("click", getPhotoBlock);
+function getPhotoBlock() {
+  leadPhoto.classList.toggle("active");
+}
+
+bunnerLabel.addEventListener("click", getBunnerBlock);
+function getBunnerBlock() {
+  leadBunner.classList.toggle("active");
+}
+
+let cityPolish = document.querySelector(".select-custom-acc-v");
